a844b459505c0b5461b9436178851804b6d0ef13
Tobias Vetter <tobiasvetter@Tobi-Mac.fritz.box>
2017-06-24 18:55:42 +0200

    Added setup page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var oauth = require('../controller/oauthController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BSH Proactive Service' });
+  res.render('index');
+});
+
+/* GET setup page. */
+router.get('/setup', function(req, res, next) {
+    res.render('setup');
 });
 
 module.exports = router;
